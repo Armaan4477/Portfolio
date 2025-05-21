590997a0c1e50fb3da038362f95b4843490e5caf
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2025-05-21 08:29:31 +0530

    trail 2.0

--- next.config.js
+++ next.config.js
@@ -6,6 +6,24 @@ const nextConfig = {
     unoptimized: true,
   },
   allowedDevOrigins: ['192.168.29.3'],
-}
 
-module.exports = nextConfig
+  // Add rewrites
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'armaan44.is-a.dev',
+            },
+          ],
+          destination: '/demos/:path*',
+        },
+      ],
+    };
+  },
+};
+
+module.exports = nextConfig;
